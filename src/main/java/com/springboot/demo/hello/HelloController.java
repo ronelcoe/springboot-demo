f58a88bf934491899497a548b499245b1ec5530e
ronelcoe <ronel_coe@yahoo.com>
2018-03-31 14:54:00 +0800

    Modified port

--- src/main/java/com/springboot/demo/hello/HelloController.java
+++ src/main/java/com/springboot/demo/hello/HelloController.java
@@ -4,19 +4,24 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-	@RequestMapping("/hello")
+	@RequestMapping(value="/hello", method = RequestMethod.GET)
 	public String hi() {
-		String hostName = "";
+		String canonicalHost = "", hostName = "", hostAddress = "";
 		try {
-			hostName = InetAddress.getLocalHost().getCanonicalHostName();
+			hostName = InetAddress.getLocalHost().getHostName();
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+			canonicalHost = InetAddress.getLocalHost().getCanonicalHostName();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-		return "hi from " + hostName;
+		return "hi from this machine. <br/>HostName: " + hostName + 
+				"<br/>HostAddress: " + hostAddress +
+				"<br/>CanonicalHostName: " + canonicalHost;
 	}
 }
