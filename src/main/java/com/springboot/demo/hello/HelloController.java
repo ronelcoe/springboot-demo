3d1bc5ef71fe89637ff727f55721d1790d436b53
ronelcoe <ronel_coe@yahoo.com>
2018-04-10 12:31:09 +0800

    removed get

--- src/main/java/com/springboot/demo/hello/HelloController.java
+++ src/main/java/com/springboot/demo/hello/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@RequestMapping(value="/hello", method = RequestMethod.GET)
+	@RequestMapping(value="/hello")
 	public String hi() {
 		String canonicalHost = "", hostName = "", hostAddress = "";
 		try {
